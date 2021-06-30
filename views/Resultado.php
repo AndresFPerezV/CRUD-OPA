f47d211fc12f518945ed93f269da38470ad2bffb
Andrés Pérez <andrespv2013@gmail.com>
2021-06-30 14:56:52 -0500

    modificamos la vista resultado ya que nos da el resultado final

--- views/Resultado.php
+++ views/Resultado.php
@@ -7,31 +7,39 @@
 	</head>
 	<body>
 	
-		<?php 
+<?php 
 			
 			foreach($datos as $listado){
 				$peso[]=$listado['peso'];
 				$calorias[]=$listado['calorias'];
 	
 			}
-
-
-
-			
+			print_r($peso);
+			print_r($calorias);
+		
 	
-		?>
+?>
 
-<script type="text/javascript">
 
+
+<script type="text/javascript" languaje="javascript">
+
+var pesoMaximo=10;
 var pesos=<?php echo json_encode($peso);?>;
 var valores=<?php echo json_encode($calorias);?>;
+//document.write("HOLA MUNDOOOOOOOOO  nose  "+"<br>");
+//document.write(valores+"<br>");
+//document.write(pesos+"<br>");
 
 
- function llenarMochila(pesoMaximo=0, pesos=[], valores=[]){
+ function llenarMochila(pesoMaximo, pesos, valores){
+	//document.write("HOLA MUNDOOOOOOOOO  nose  "+"<br>");
     let values = [];
+	//document.write("HOLA MUNDOOOOOOOOO check "+"<br>");
     //TABLA DE RESULTADOS INTERMEDIOS: Valores máximos
-    numIter++;
+    var numIter=0;
     values = pesos.map(v => Array.from({length:  pesoMaximo+1}, () => 0));
+	//document.write("HOLA MUNDOOOOOOOOO  nose  "+"<br>");
     for (let i=1; i<pesos.length; i++) {
         numIter++;
         for (let j=1; j<=pesoMaximo; j++){
@@ -51,20 +59,19 @@ var valores=<?php echo json_encode($calorias);?>;
     for (let i=pesos.length-1; i>0; i--){
         numIter++;
         if (values[i][j] !== values[i-1][j] && values[i][j] === values[i-1][j-pesos[i]] + valores[i]){
+			//document.write(pesos[ï]+values[i][j]+"Respuesta");
             objetos.push(i);
             j -= pesos[i];
         }
     }
-	document.write("HOLA MUNDOOOOOOOOO");
+
+	document.write(objetos+"final");
+	
     //return objetos;
 }
+//document.write("HOLA MUNDOOOOOOOOO");
+llenarMochila(pesoMaximo,pesos,valores);
 </script>
-	
-
-	
-	
-           
- 
 	
  
  
